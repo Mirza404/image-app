cafdef251f384933b084b8e3383bbb31adb13c69
Mirza404 <mirzabdulahovic@gmail.com>
2023-10-02 12:29:44 +0200

    Fix: remove env

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-const accessKey = process.env.ACCESS_KEY;
+const accessKey = "xRAgaFlvjD-Ga9Scadz-BW-2gMN3jtIwY1QhM9auQjs"
 
-const formEl = document.querySelector("form");
-const inputEl = document.getElementById("search-input");
-const searchResults = document.querySelector(".search-results");
-const showMoreButton = document.getElementById("show-more-button");
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMoreButton = document.getElementById("show-more-button")
 
-let inputData = "";
+let inputData = ""; 
 let page = 1;
 
-async function searchImages() {
-  inputData = inputEl.value;
-  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+async function searchImages(){
+    inputData = inputEl.value 
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`; 
 
-  const response = await fetch(url);
-  const data = await response.json();
+    const response = await fetch(url) 
+    const data = await response.json() 
 
-  const results = data.results;
+    const results = data.results 
 
-  if (page === 1) {
-    searchResults.innerHTML = "";
-  }
+    if (page === 1){
+        searchResults.innerHTML = ""
+    }
 
-  results.map((result) => {
-    const imageWrapper = document.createElement("div");
-    imageWrapper.classList.add("search-result");
-    const image = document.createElement("img");
-    image.src = result.urls.small;
-    image.alt = result.alt_description;
-    const imageLink = document.createElement("a");
-    imageLink.href = result.links.html;
-    imageLink.target = "_blank";
-    imageLink.textContent = result.alt_description;
+    results.map((result) =>{
+        const imageWrapper = document.createElement('div') 
+        imageWrapper.classList.add("search-result") 
+        const image = document.createElement('img') 
+        image.src = result.urls.small 
+        image.alt = result.alt_description 
+        const imageLink = document.createElement('a') 
+        imageLink.href = result.links.html 
+        imageLink.target = "_blank" 
+        imageLink.textContent = result.alt_description 
 
-    imageWrapper.appendChild(image);
-    imageWrapper.appendChild(imageLink);
-    searchResults.appendChild(imageWrapper);
-  });
+        imageWrapper.appendChild(image) 
+        imageWrapper.appendChild(imageLink) 
+        searchResults.appendChild(imageWrapper) 
+    })
 
-  page++;
-  if (page > 1) {
-    showMoreButton.style.display = "block";
-  }
+    page++ 
+    if(page>1){
+        showMoreButton.style.display = "block"; 
+    }
 }
 
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  page = 1;
-  searchImages();
-});
+formEl.addEventListener("submit", (event) =>{
+    event.preventDefault(); 
+    page = 1; 
+    searchImages(); 
+})
 
-showMoreButton.addEventListener("click", () => {
-  searchImages();
-});
+showMoreButton.addEventListener("click", () =>{
+    searchImages(); 
+})
